2c4350f7d11d37e1c2bea111d5e412968e6ebe05
Victor Ermolaev <victorermolaev@gmail.com>
2020-06-12 16:01:08 +0200

    Update two examples

--- examples/example.rs
+++ examples/example.rs
@@ -1,25 +1,42 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 extern crate trace;
-
 use trace::trace;
 
-trace::init_depth_var!();
-
 fn main() {
-    foo(1, 2);
+    env_logger::init();
+
+    // foo(1, 2);
+    let a = A::new().unwrap();
+    a.test(1, 2);
 }
 
-#[trace]
+// #[trace(a = "--{}--", disable(b))]
 fn foo(a: i32, b: i32) {
     println!("I'm in foo!");
     bar((a, b));
 }
 
-#[trace(prefix_enter="[ENTER]", prefix_exit="[EXIT]")]
+// #[trace(disable(a, b), res = "result: {:?}")]
 fn bar((a, b): (i32, i32)) -> i32 {
     println!("I'm in bar!");
     if a == 1 {
         2
     } else {
         b
+    }
+}
+
+#[derive(Debug)]
+struct A {}
+
+#[trace(prefix_enter = "A::", prefix_exit = "A::", disable(new))]
+impl A {
+    fn new() -> Result<Self, u32> {
+        Ok(Self {})
+    }
+    #[trace(prefix_enter = "<method>::", a = "--{}--", disable(b))]
+    fn test(&self, a: u32, b: u32) -> u32 {
+        10
     }
 }
--- examples/example_mod.rs
+++ examples/example_mod.rs
@@ -2,7 +2,7 @@
 // error[E0658]: The attribute `trace` is currently unknown to the compiler and may have meaning added to it in the future (see issue #29642)
 //#![trace]
 
-#![feature(proc_macro_hygiene)]  // to use custom attributes on `mod`
+// #![feature(proc_macro_hygiene)]  // to use custom attributes on `mod`
 
 extern crate trace;
 
@@ -13,20 +13,22 @@ use trace::trace;
 //#![trace]
 
 fn main() {
+    env_logger::init();
+
     foo::foo();
     let foo = foo::Foo;
     foo.bar();
 }
 
-#[trace]
-mod foo{
+#[trace(prefix_enter = "foo::", prefix_exit = "foo::")]
+mod foo {
     pub(super) fn foo() {
         println!("I'm in foo!");
     }
 
     pub(super) struct Foo;
+    #[trace(prefix_enter = "Foo::", prefix_exit = "Foo::")]
     impl Foo {
-        pub(super) fn bar(&self) {
-        }
+        pub(super) fn bar(&self) {}
     }
 }
